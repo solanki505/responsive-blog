d93690f6555c72a4cd48271a87d035154a90d1be
Solanki Sarkar <158484284+solanki505@users.noreply.github.com>
2024-06-25 11:35:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("body").classList.add("loaded");
+    }, 500)
+});
 
